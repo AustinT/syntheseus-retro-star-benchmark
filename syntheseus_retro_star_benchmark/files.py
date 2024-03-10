499d43177f30de96a417c76e4880e5d9444a99f3
Austin Tripp <ajt212@cam.ac.uk>
2024-03-10 14:54:36 +0000

    Initial code dump.

---
+++ syntheseus_retro_star_benchmark/files.py
@@ -0,0 +1,42 @@
+"""Code to handle retrieving all files."""
+
+from pathlib import Path
+
+from syntheseus.reaction_prediction.utils.downloading import get_cache_dir
+
+BASE_PATH = get_cache_dir("syntheseus_retro_star_benchmark")
+
+
+def get_test_routes_file() -> Path:
+    expected_path = BASE_PATH / "190-hard-test-routes.pkl"
+    if not expected_path.exists():
+        raise NotImplementedError  # TODO
+    return expected_path
+
+
+def get_value_function_checkpoint() -> Path:
+    expected_path = BASE_PATH / "value-function-checkpoint.pt"
+    if not expected_path.exists():
+        raise NotImplementedError  # TODO
+    return expected_path
+
+
+def get_rxn_model_checkpoint() -> Path:
+    expected_path = BASE_PATH / "pretrained-reaction-model.ckpt"
+    if not expected_path.exists():
+        raise NotImplementedError  # TODO
+    return expected_path
+
+
+def get_template_file() -> Path:
+    expected_path = BASE_PATH / "template_rules_1.dat"
+    if not expected_path.exists():
+        raise NotImplementedError  # TODO
+    return expected_path
+
+
+def get_original_inventory_csv() -> Path:
+    expected_path = BASE_PATH / "ORIGINAL-inventory.csv.gz"
+    if not expected_path.exists():
+        raise NotImplementedError  # TODO
+    return expected_path
